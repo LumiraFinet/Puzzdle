3d5fe329ddc65c340dc30e815a0bbf6b52d27022
LumiraFinet <navysaberram@gmail.com>
2025-11-05 19:05:46 -0600

    Update page.js
    This requires route.js to work. Using both, it reads from a txt file.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,31 @@
-import Image from "next/image";
+//Needs another file called route.js to work. The path should be /Users/analyticsautomations/Puzzle/puzzdle/app/api/readfile
+//The txt file route.js is reading from is called myfile.txt
 
-export default function Home() {
+'use client';            // runs in the browser
+
+import { useEffect, useState } from 'react';
+
+export default function H() {
+  const [fileText, setFileText] = useState('');
+
+  useEffect(() => {
+    // ask our API route for the file text
+	fetch('/api/readfile')
+	.then(res => res.json())
+	.then(data => {
+	  if (data.error) {
+		console.error('Error:', data.error);
+	  } else {
+		console.log('File content (browser):', data.text);
+		setFileText(data.text);
+	  }
+	});
+  }, []);
+
   return (
     <div>
-      <h1> THis is a header </h1>
-      <p> this is a paragraph </p>
+      <h2>File content from the server:</h2>
+      <pre>{fileText || 'Loading...'}</pre>
     </div>
   );
 }
